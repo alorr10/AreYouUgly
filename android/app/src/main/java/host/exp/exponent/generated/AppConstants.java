f47dfad1eebd06e8ffd4d0a3ec29a930b58320b1
Alec Lorraine <alec@brighten.in>
2019-01-14 14:33:24 -0800

    add everything else

---
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -0,0 +1,65 @@
+package host.exp.exponent.generated;
+
+import com.facebook.common.internal.DoNotStrip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import host.exp.exponent.BuildConfig;
+import host.exp.exponent.Constants;
+
+@DoNotStrip
+public class AppConstants {
+
+  public static final String VERSION_NAME = "2.10.3";
+  public static String INITIAL_URL = "exp://exp.host/@aleclorraine/AreYouUgly";
+  public static final String SHELL_APP_SCHEME = "exp36f572ad629d4a74a3816aebb24a9149";
+  public static final String RELEASE_CHANNEL = "default";
+  public static boolean SHOW_LOADING_VIEW_IN_SHELL_APP = false;
+  public static boolean ARE_REMOTE_UPDATES_ENABLED = true;
+  public static final List<Constants.EmbeddedResponse> EMBEDDED_RESPONSES;
+  public static boolean FCM_ENABLED = false;
+
+  static {
+    List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
+
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@aleclorraine/AreYouUgly", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40aleclorraine%2FAreYouUgly%2F1.0.0%2F35c531f5a25e0dc1184b4fe1db7b6558-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
+    EMBEDDED_RESPONSES = embeddedResponses;
+  }
+
+  // Called from expoview/Constants
+  public static Constants.ExpoViewAppConstants get() {
+    Constants.ExpoViewAppConstants constants = new Constants.ExpoViewAppConstants();
+    constants.VERSION_NAME = VERSION_NAME;
+    constants.INITIAL_URL = INITIAL_URL;
+    constants.SHELL_APP_SCHEME = SHELL_APP_SCHEME;
+    constants.RELEASE_CHANNEL = RELEASE_CHANNEL;
+    constants.SHOW_LOADING_VIEW_IN_SHELL_APP = SHOW_LOADING_VIEW_IN_SHELL_APP;
+    constants.ARE_REMOTE_UPDATES_ENABLED = ARE_REMOTE_UPDATES_ENABLED;
+    constants.EMBEDDED_RESPONSES = EMBEDDED_RESPONSES;
+    constants.ANDROID_VERSION_CODE = BuildConfig.VERSION_CODE;
+    constants.FCM_ENABLED = FCM_ENABLED;
+    return constants;
+  }
+}
