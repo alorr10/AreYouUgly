4778e2ee931adb6d852758cf14d06ba3b618e1c2
Alec Lorraine <alec@brighten.in>
2019-01-14 19:21:56 -0800

    ota

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -40,10 +40,11 @@ public class AppConstants {
         
         
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@aleclorraine/AreYouUgly", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40aleclorraine%2FAreYouUgly%2F1.0.0%2F35c531f5a25e0dc1184b4fe1db7b6558-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40aleclorraine%2FAreYouUgly%2F1.0.0%2F64382fa2b996aa358435d0c0d30621ee-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
